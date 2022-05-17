64ec354d5991c1b5ff3911738e735c417bbc8a20
Brian Jomo <98019316+BrianJomo@users.noreply.github.com>
2022-05-17 12:24:49 +0300

    This will remove some typing errors in the file.

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,7 +19,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "perfect pitches login"
+    title = "Pitchico login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -30,7 +30,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to perfect pitches","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Pitchico","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
 
     title = "New Account"
